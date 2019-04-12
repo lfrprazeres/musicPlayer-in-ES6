68add32581a3357926f1be37256294fb37aa9296
Luiz Fernando <lfrprazeres@gmail.com>
2019-04-12 12:44:16 -0300

    some changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 
 window.addEventListener("load",() => {
+    let controlButtons = document.querySelector(".controlButtons")
     let container = document.querySelector(".musics-container")
+    let stopButton = document.querySelector(".stopButton")
+    let pauseButton = document.querySelector(".playerButton")
+    let currentMusicIndex = 0
     let album ={
         musics: [{
             music: "CountingCrows",
@@ -10,7 +14,8 @@ window.addEventListener("load",() => {
             label: "Matchbox Twenty - Push"
         }]
     }
-    for(music of album.musics){
+
+    album.musics.map((music,key) => {
         // Set music container
         let musicContainer = document.createElement("div")
         musicContainer.className = "music-container"
@@ -18,13 +23,38 @@ window.addEventListener("load",() => {
         // Set Audio
         let sound = document.createElement("audio")
         sound.src = "./musics/" + music.music + ".mp3"
+        sound.className = "music" + key
 
         // Set playButton
         let playButton = document.createElement("button")
-        playButton.className = "playButton"
+        playButton.className = "playButton play" + key
         playButton.innerHTML = '<i class="fas fa-play"></i>Tocar'
+        playButton.addEventListener("mouseover",() => {
+            musicContainer.style.backgroundColor = "#754F3D"
+        })
+        playButton.addEventListener("mouseout",() => {
+            musicContainer.style.backgroundColor = "transparent"
+        })
+        
         playButton.addEventListener("click",() => {
-            sound.play();
+            sound.play()
+            currentMusicIndex = key;
+            pauseButton.innerHTML = '<i class="fas fa-pause"></i>Pausar'
+            pauseButton.className = "playerButton pauseButton"
+            if(document.querySelector(".musicName") === null){
+                let labelName = document.createElement("label")
+                labelName.className = "musicName"
+                labelName.innerHTML = "Tocando: " + album.musics[currentMusicIndex].label
+                controlButtons.appendChild(labelName)
+            } else {
+                document.querySelector(".musicName").innerHTML = "Tocando: " + album.musics[currentMusicIndex].label
+            }
+            album.musics.map((item,key) => {
+                if(key === currentMusicIndex)
+                    return
+                document.querySelector(".music" + key).pause()
+                document.querySelector(".music" + key).currentTime = 0
+            })
         })
 
         // Add music container into pattern
@@ -36,6 +66,46 @@ window.addEventListener("load",() => {
         // Set the music container's child
         musicContainer.appendChild(sound)
         musicContainer.appendChild(playButton)
-        musicContainer.appendChild(stopButton)
+
+    })
+
+    stopButton.addEventListener("click", () => {
+        let currentMusic = document.querySelector(".music" + currentMusicIndex)
+        currentMusic.currentTime = 0
+        if(document.querySelector(".musicName") !== null)
+            controlButtons.removeChild(document.querySelector(".musicName"))
+        currentMusic.pause()
+        
+    })
+
+    pauseMusic = () => {
+        document.querySelector(".music" + currentMusicIndex).pause()
     }
+
+    playMusic = () => {
+        if(document.querySelector(".musicName") === null){
+            let labelName = document.createElement("label")
+            labelName.className = "musicName"
+            labelName.innerHTML = "Tocando: " + album.musics[currentMusicIndex].label
+            controlButtons.appendChild(labelName)
+        } else {
+            document.querySelector(".musicName").innerHTML = "Tocando: " + album.musics[currentMusicIndex].label
+        }
+        document.querySelector(".music" + currentMusicIndex).play()
+    }
+
+
+    pauseButton.addEventListener("click", () => {
+            let currentMusic = document.querySelector(".music" + currentMusicIndex)
+            if(pauseButton.className === "playerButton pauseButton"){
+                pauseButton.innerHTML = '<i class="fas fa-play"></i>Tocar'
+                pauseButton.className = "playerButton playButton"
+                pauseButton.addEventListener("click", this.playMusic)
+            } else {
+                pauseButton.innerHTML = '<i class="fas fa-pause"></i>Pausar'
+                pauseButton.className = "playerButton pauseButton"
+                pauseButton.addEventListener("click", this.pauseMusic)
+            }
+        
+    })
 })
