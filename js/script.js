31b1694b1f6d811565d74ff77281d02e58edaf42
Luiz Fernando <lfrprazeres@gmail.com>
2019-04-26 17:44:21 -0300

    next and previous button added

--- js/script.js
+++ js/script.js
@@ -1,149 +1,159 @@
 
 
+let container = document.querySelector(".musics-container")
+let playerfooterButton = document.querySelector(".playerButton.footerButton")
+let footer = document.querySelector("footer")
+let nextButton = document.querySelector(".nextButton")
+let stopButton = document.querySelector(".stopButton")
+let currentMusicIndex = 0
+let album = {
+    musics: [{
+        music: "CountingCrows",
+        label: "Counting Crows - Mr Jones"
+    }, {
+        music: "MatchboxTwenty",
+        label: "Matchbox Twenty - Push"
+    }]
+}
 
-window.addEventListener("load", () => {
-    let container = document.querySelector(".musics-container")
-    let playerfooterButton = document.querySelector(".playerButton.footerButton")
-    let stopButton = document.querySelector(".stopButton")
-    let currentMusicIndex = 0
-    let album = {
-        musics: [{
-            music: "CountingCrows",
-            label: "Counting Crows - Mr Jones"
-        },{
-            music: "MatchboxTwenty",
-            label: "Matchbox Twenty - Push"
-        }]
-    }
+class Music {
+    constructor() {
+        // Creating musicContainer
+        this.musicContainer = document.createElement("div")
+        this.musicContainer.className = "music-container"
 
-    class Music {
-        constructor() {
-            // Creating musicContainer
-            this.musicContainer = document.createElement("div")
-            this.musicContainer.className = "music-container"
+        // Creating sound
+        this.sound = document.createElement("audio")
 
-            // Creating sound
-            this.sound = document.createElement("audio")
+        // Creating play Button
+        this.button = document.createElement("button")
+        this.button.innerHTML = '<i class="fas fa-play"></i>'
 
-            // Creating play Button
-            this.button = document.createElement("button")
-            this.button.innerHTML = '<i class="fas fa-play"></i>Tocar'
+        // seting hover
+        this.button.addEventListener("mouseover", () => {
+            this.musicContainer.style.backgroundColor = "#333"
+        })
+        this.button.addEventListener("mouseout", () => {
+            this.musicContainer.style.backgroundColor = "transparent"
+        })
 
-            // seting hover
-            this.button.addEventListener("mouseover", () => {
-                this.musicContainer.style.backgroundColor = "#754F3D"
-            })
-            this.button.addEventListener("mouseout", () => {
-                this.musicContainer.style.backgroundColor = "transparent"
-            })
+        // validation to playing
+        this.playing = false
 
-            // validation to playing
-            this.playing = false
+        // creating a p to put the music name
+        this.musicName = document.createElement("p")
 
-            // creating a p to put the music name
-            this.musicName = document.createElement("p")
+        this.musicContainer.appendChild(this.musicName)
+        this.musicContainer.appendChild(this.button)
+        this.musicContainer.appendChild(this.sound)
 
-            this.musicContainer.appendChild(this.musicName)
-            this.musicContainer.appendChild(this.button)
-            this.musicContainer.appendChild(this.sound)
+        // the key(number) of the music
+        this.key
 
-            // the key(number) of the music
-            this.key
+        // set button to play or pause the music
+        this.button.addEventListener("click", (e) => {
+            if (footer.style.display == "" || footer.style.display == "none") {
+                footer.style.display = "flex"
+            }
 
-            // set button to play or pause the music
-            this.button.addEventListener("click", (e) => {
-                if(document.querySelector(".footerButton").style.display == ""){
-                    Array.prototype.map.call(document.querySelectorAll(".footerButton"), (item) => {
-                        item.style = "display: flex"
-                    })
-                }
-                
-                if(this.playing === false){
-                    // Reset previous button and pause previous music
-                   if(currentMusicIndex !== this.key){
+            if (this.playing === false) {
+                // Reset previous button and pause previous music
+                if (currentMusicIndex !== this.key) {
                     let previousMusic = document.querySelector(".music" + currentMusicIndex)
                     previousMusic.pause()
                     previousMusic.currentTime = 0
                     let previousButton = previousMusic.parentElement.querySelector("button")
                     previousButton.className = this.button.className = "playerButton playButton"
-                    previousButton.innerHTML = this.button.innerHTML = '<i class="fas fa-play"></i>Tocar'
+                    previousButton.innerHTML = this.button.innerHTML = '<i class="fas fa-play"></i>'
                     currentMusicIndex = this.key
-                   }
-                    // play the music
-                    e.target.parentElement.querySelector("audio").play()
-                    this.playing = !this.playing
-                    this.button.innerHTML = '<i class="fas fa-pause"></i>Pausar'
-                    this.button.className = "playerButton pauseButton"
-
-                    playerfooterButton.innerHTML = '<i class="fas fa-pause"></i>Pausar'
-                    playerfooterButton.className = "playerButton pauseButton footerButton"
-
-                    // show the music name in the footer
-                    document.querySelector(".musicName").innerHTML = "Tocando: " + album.musics[currentMusicIndex].label
-                    
-                } else {
-                    e.target.parentElement.querySelector("audio").pause()
-                    this.button.innerHTML = '<i class="fas fa-play"></i>Tocar'
-                    this.button.className = "playerButton playButton"
-                    this.playing = !this.playing
-
-                    playerfooterButton.innerHTML = '<i class="fas fa-play"></i>Tocar'
-                    playerfooterButton.className = "playerButton playButton footerButton"
                 }
-            })
-        }
+                // play the music
+                e.target.parentElement.querySelector("audio").play()
+                this.playing = !this.playing
+                this.button.innerHTML = '<i class="fas fa-pause"></i>'
+                this.button.className = "playerButton pauseButton"
 
-        init = (music, name, key) => {
-            // set the key(number) of the music
-            this.key = key
+                playerfooterButton.innerHTML = '<i class="fas fa-pause"></i>'
+                playerfooterButton.className = "playerButton pauseButton footerButton"
 
-            // set the sound tag className and source
-            this.sound.className = "music" + key
-            this.sound.src = "./musics/" + music + ".mp3"
+                // show the music name in the footer
+                document.querySelector(".musicName").innerHTML = "Tocando: " + album.musics[currentMusicIndex].label
 
-            //Set PlayButton className
-            this.button.className = "playButton play" + key
+            } else {
+                e.target.parentElement.querySelector("audio").pause()
+                this.button.innerHTML = '<i class="fas fa-play"></i>'
+                this.button.className = "playerButton playButton"
+                this.playing = !this.playing
 
-            // put a name in the musicContainer
-            this.musicName.innerHTML = name
-
-            // Add music container into pattern
-            container.appendChild(this.musicContainer)
-        }
-
+                playerfooterButton.innerHTML = '<i class="fas fa-play"></i>'
+                playerfooterButton.className = "playerButton playButton footerButton"
+            }
+        })
     }
 
-    album.musics.map((sound, key) => {
-        let music = new Music()
-        music.init(sound.music, sound.label, key)
-    })
+    init = (music, name, key) => {
+        // set the key(number) of the music
+        this.key = key
 
-    playerfooterButton.addEventListener("click", () => {
-        let currentMusic = document.querySelector(".music" + currentMusicIndex)
-        // validation: if music is playing, pause it
-        if(!currentMusic.paused){
-            currentMusic.pause()
-            playerfooterButton.innerHTML = '<i class="fas fa-play"></i>Tocar'
-            playerfooterButton.className = "playerButton playButton footerButton"
-        } else {
-            currentMusic.play()
-            playerfooterButton.innerHTML = '<i class="fas fa-pause"></i>Pausar'
-            playerfooterButton.className = "playerButton pauseButton footerButton"
-        }
+        // set the sound tag className and source
+        this.sound.className = "music" + key
+        this.sound.src = "./musics/" + music + ".mp3"
 
-    })
+        //Set PlayButton className
+        this.button.className = "playButton play" + key
 
-    // set the click of stop button placed in the footer
-    stopButton.addEventListener("click", () => {
-        let currentMusic = document.querySelector(".music" + currentMusicIndex)
-        let currentButton = currentMusic.parentElement.querySelector("button")
-        if(!currentMusic.paused){
-            currentButton.className = "playerButton playButton"
-            currentButton.innerHTML = '<i class="fas fa-play"></i>Tocar'
-            playerfooterButton.innerHTML = '<i class="fas fa-play"></i>Tocar'
-            playerfooterButton.className = "playerButton playButton footerButton"
-        }
-        currentMusic.currentTime = 0
-        currentMusic.pause()
-    })
+        // put a name in the musicContainer
+        this.musicName.innerHTML = name
+
+        // Add music container into pattern
+        container.appendChild(this.musicContainer)
+    }
+
+}
+
+// init the musics
+album.musics.map((sound, key) => {
+    let music = new Music()
+    music.init(sound.music, sound.label, key)
 })
+
+// set the click event to pause or play the current music in the footer's player button
+playerfooterButton.addEventListener("click", () => {
+    let currentMusic = document.querySelector(".music" + currentMusicIndex)
+    let currentButton = currentMusic.parentElement.querySelector("button")
+    // validation: if music is playing, pause it
+    if (!currentMusic.paused) {
+        currentMusic.pause()
+        playerfooterButton.innerHTML = '<i class="fas fa-play"></i>'
+        playerfooterButton.className = "playerButton playButton footerButton"
+        currentButton.innerHTML = '<i class="fas fa-play"></i>'
+        currentButton.className = "playerButton playButton footerButton"
+
+    } else {
+        currentMusic.play()
+        playerfooterButton.innerHTML = '<i class="fas fa-pause"></i>'
+        playerfooterButton.className = "playerButton pauseButton footerButton"
+        currentButton.innerHTML = '<i class="fas fa-pause"></i>'
+        currentButton.className = "playerButton pauseButton footerButton"
+    }
+
+})
+
+// set the click of stop button placed in the footer
+stopButton.addEventListener("click", () => {
+    // catching current Music
+    let currentMusic = document.querySelector(".music" + currentMusicIndex)
+    // catching the current music's button
+    let currentButton = currentMusic.parentElement.querySelector("button")
+    // validation: if current music is playing
+    currentButton.className = "playerButton playButton"
+    currentButton.innerHTML = '<i class="fas fa-play"></i>'
+    footer.style.display = "none"
+    currentMusic.pause()
+    currentMusic.currentTime = 0
+})
+
+
+// want to fix
+console.log(document.querySelector(".musicName").style.width)
+document.querySelector(".musicName").style.width = `calc(100% - ${window.getComputedStyle(document.querySelector(".controlButtons")).width}`
