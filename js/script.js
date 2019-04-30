55ec5d8f5adcc9122ad5e846f6ff5da70ca1a2f0
Luiz Fernando <lfrprazeres@gmail.com>
2019-04-30 16:43:30 -0300

    change the control buttons to right and set a vinyl gif when the music is playing

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ let footer = document.querySelector("footer")
 let nextButton = document.querySelector(".nextButton")
 let previousButton = document.querySelector(".previousButton")
 let stopButton = document.querySelector(".stopButton")
+let vinylGif = document.querySelector(".vinylContainer img")
 let currentMusicIndex = 0
 let album = {
     musics: []
@@ -13,23 +14,18 @@ let album = {
 
 let blob = window.URL || window.webkitURL;
 
-filesChooser.addEventListener("change", (e) => {
-    Array.from(e.target.files).forEach(audio => {
+filesChooser.addEventListener("change", (files) => {
+    Array.from(files.target.files).forEach(audio => {
         fileURL = blob.createObjectURL(audio);
+        // remove the type of the file (example: someAudio.mp3 become someAudio)
+        let realName = audio.name.substring(0,audio.name.length - 4)
         const music = {
             music: fileURL,
-            label: audio.name
+            label: realName
         }
         album.musics.push(music)
-        console.log(fileURL)
-        // console.log(album.musics)
-        // let test = document.createElement("audio")
-        // test.className = "test"
-        // container.appendChild(test)
-        // test.src = fileURL
-        // test.play()
-        // init the musics
     })
+    // init the musics
     document.querySelector(".chooseDirectoryContainer").style.display = "none"
     album.musics.map((sound, key) => {
         let music = new Music()
@@ -52,6 +48,8 @@ function play() {
     playerfooterButton.innerHTML = '<i class="fas fa-pause"></i>'
     // show the music name in the footer
     document.querySelector(".musicName").innerHTML = "Tocando: " + album.musics[currentMusicIndex].label
+    vinylGif.src = "./gif/vinyl.gif"
+    
 }
 
 // function to update the footer line (music duration)
@@ -79,6 +77,7 @@ function pause() {
     currentButton.className = "playerButton playButton footerButton"
     playerfooterButton.className = "playerButton playButton"
     playerfooterButton.innerHTML = '<i class="fas fa-play"></i>'
+    vinylGif.src = "./img/vinyl.png"
 }
 
 
@@ -217,8 +216,3 @@ nextButton.addEventListener("click", () => {
     }
     play()
 })
-
-
-// need to fix
-console.log(document.querySelector(".musicName").style.width)
-document.querySelector(".musicName").style.width = `calc(100% - ${window.getComputedStyle(document.querySelector(".controlButtons")).width}`
